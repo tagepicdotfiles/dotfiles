874de07a671ae7c3e036073da42bfaa294311487
TAG-Epic <tagepicuwu@gmail.com>
2023-07-06 00:43:43 +0200

    remove old polybar config

--- polybar/scripts/get_volume.sh
+++
@@ -1,1 +0,0 @@
-awk -F"[][]" '/Left:/ { print $2 }' <(amixer sget Master)
--- polybar/startbar.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --config=$HOME/.config/polybar/polybar.ini -r main &
-done
-
-# polybar --config=$HOME/.config/polybar/polybar.ini main &
