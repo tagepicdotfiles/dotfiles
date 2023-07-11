8c2f6d7e58c00696f8f22874429b10f500f5aace
TAG-Epic <tagepicuwu@gmail.com>
2023-07-11 02:15:17 +0200

    more dep hell

--- flake.nix
+++ flake.nix
@@ -4,6 +4,26 @@
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
         nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
         flake-utils.url = "github:numtide/flake-utils";
+        flake-compat = {
+            url = "github:edolstra/flake-compat";
+            flake = false;
+        };
+        rust-overlay = {
+            url = "github:oxalica/rust-overlay";
+            inputs = {
+                flake-utils.follows = "flake-utils";
+                nixpkgs.follows = "nixpkgs";
+            };
+        };
+        crane = {
+            url = "github:ipetkov/crane";
+            inputs = {
+                flake-utils.follows = "flake-utils";
+                flake-compat.follows = "flake-compat";
+                nixpkgs.follows = "nixpkgs";
+                rust-overlay.follows = "rust-overlay";
+            };
+        };
 
 
         home-manager = {
@@ -21,6 +41,7 @@
             inputs = {
                 nixpkgs.follows = "nixpkgs";
                 flake-utils.follows = "flake-utils";
+                crane.follows = "crane";
             };
         };
         btop-gpu = {
@@ -28,6 +49,7 @@
             inputs = {
                 # TODO: Uncomment once the stable version of "fmt" is 10.0.0
                 #nixpkgs.follows = "nixpkgs";
+                flake-utils.follows = "flake-utils";
                 nixpkgs.follows = "nixpkgs-unstable";
             };
         };
