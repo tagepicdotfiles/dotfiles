7bdd064bb0d973d290ed5207b682924c25db01e0
TAG-Epic <tagepicuwu@gmail.com>
2024-01-17 00:30:11 +0100

    port forward

--- overrides/nidoran/caddy.nix
+++ overrides/nidoran/caddy.nix
@@ -8,4 +8,13 @@ in {
             reverse_proxy http://localhost:${toString ports.data-robbery.http}
         '';
     };
+    networking.firewall.allowedTCPPorts = [
+        80
+        443
+    ];
+    networking.firewall.allowedUDPPorts = [
+        80
+        443
+    ];
+
 }
