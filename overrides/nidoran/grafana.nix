2cdc082163826150f6e31e8132d229e61821f466
TAG-Epic <tagepicuwu@gmail.com>
2024-03-23 00:27:05 +0100

    refactor(nidoran): switch to victoriametrics

--- overrides/nidoran/grafana.nix
+++ overrides/nidoran/grafana.nix
@@ -2,20 +2,47 @@
 {...}: let
     ports = import ./ports.nix;
 in {
-    services.prometheus = {
+    services.victoriametrics = {
         enable = true;
-        port = ports.utils.prometheus;
+        retentionPeriod = "1y";
+        listenAddress = ":${ports.utils.prometheus}";
+        extraOptions = ["-promscrape.config=/etc/victoriametrics-scrape.yaml"];
+    };
+    environment.etc = {
+        "victoriametrics-scrape.yaml" = ''
+        # Sample config for Prometheus.
+        global:
+          scrape_interval:     15s # Set the scrape interval to every 15 seconds. Default is every 1 minute.
+          evaluation_interval: 15s # Evaluate rules every 15 seconds. The default is every 1 minute.
+          # scrape_timeout is set to the global default (10s).
 
-        scrapeConfigs = [
-            {
-                job_name = "vail-scraper";
-                static_configs = [
-                    {
-                        targets = ["localhost:${toString ports.vailScraper.scraper}"];
-                    }
-                ];
-            }
-        ];
+          # Attach these labels to any time series or alerts when communicating with
+          # external systems (federation, remote storage, Alertmanager).
+          #  external_labels:
+          #    monitor: 'example'
+
+        # Alertmanager configuration
+        #alerting:
+        #  alertmanagers:
+        #  - static_configs:
+        #    - targets: ['localhost:9093']
+
+        # Load rules once and periodically evaluate them according to the global 'evaluation_interval'.
+        #rule_files:
+          # - "first_rules.yml"
+          # - "second_rules.yml"
+
+        # A scrape configuration containing exactly one endpoint to scrape:
+        # Here it's Prometheus itself.
+        scrape_configs:
+          # The job name is added as a label `job=<job_name>` to any timeseries scraped from this config.
+          - job_name: 'vail-scraper'
+
+            static_configs:
+              - targets: [
+                "localhost:${toString ports.vailScraper.scraper}" # filserver
+              ]
+        ''
     };
     services.grafana = {
         enable = true;
