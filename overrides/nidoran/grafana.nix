78710cd5456557eeea293762f2a34e0240a178f4
TAG-Epic <tagepicuwu@gmail.com>
2024-03-23 00:37:30 +0100

    h

--- overrides/nidoran/grafana.nix
+++ overrides/nidoran/grafana.nix
@@ -13,7 +13,6 @@ in {
         # Sample config for Prometheus.
         global:
           scrape_interval:     15s # Set the scrape interval to every 15 seconds. Default is every 1 minute.
-          evaluation_interval: 15s # Evaluate rules every 15 seconds. The default is every 1 minute.
           # scrape_timeout is set to the global default (10s).
 
           # Attach these labels to any time series or alerts when communicating with
@@ -38,7 +37,6 @@ in {
           # The job name is added as a label `job=<job_name>` to any timeseries scraped from this config.
           - job_name: 'vail-scraper'
             scrape_interval:     15s # Set the scrape interval to every 15 seconds. Default is every 1 minute.
-            evaluation_interval: 15s # Evaluate rules every 15 seconds. The default is every 1 minute.
 
             static_configs:
               - targets: [
