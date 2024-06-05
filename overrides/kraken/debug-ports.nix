0ff6ee336ca936837f652b764d0aa31a2a99d2ce
TAG-Epic <tagepicuwu@gmail.com>
2024-06-05 20:21:44 +0200

    feat: htb revshell port

--- overrides/kraken/debug-ports.nix
+++ overrides/kraken/debug-ports.nix
@@ -4,6 +4,7 @@
     networking.firewall.allowedTCPPorts = [
         5173 # Svelte apps - for debugging
         8972 # HTB
+        8971 # HTB revshell
     ];
     networking.firewall.allowedUDPPorts = [];
 }
